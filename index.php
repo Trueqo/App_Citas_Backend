a9f1234057cfb4dc58eedecff74f898670c6d19f
christian lazo <kenishi.cristianxd@gmail.com>
2022-03-27 23:18:11 -0500

    Add method Edit

--- index.php
+++ index.php
@@ -8,9 +8,18 @@
     $service = "localhost"; $user = "root"; $password = ""; $nameDatabase = "appcitas";
     $connectionBD = new mysqli($service, $user, $password, $nameDatabase);
 
+    //Buscar
+    if (isset($_GET["search"])){
+        $sqlApointment = mysqli_query($connectionBD,"SELECT * FROM citas WHERE id=".$_GET["search"]);
+        if(mysqli_num_rows($sqlApointment) > 0){
+            $dataApointment = mysqli_fetch_all($sqlApointment,MYSQLI_ASSOC);
+            echo json_encode($dataApointment);
+            exit();
+        }
+        else{  echo json_encode(["success"=>0]); }
+    }
 
-
-    //Crea un Cita
+    //Crear
     if(isset($_GET["create"])){
         $data = json_decode(file_get_contents("php://input"));
         $name=$data->name;
@@ -38,7 +47,22 @@
         else{  echo json_encode(["success"=>0]); }
     }
 
-
+    //Actualizar
+    if(isset($_GET["update"])){
+    
+        $data = json_decode(file_get_contents("php://input"));
+    
+        $id=(isset($data->id))?$data->id:$_GET["update"];
+        $name=$data->name;
+        $topic=$data->topic;
+        $date=$data->date;
+        $hour=$data->hour;
+        $currentTime=$data->currentTime;
+        
+        $sqlApointment = mysqli_query($connectionBD,"UPDATE citas SET name='$name',topic='$topic', date='$date', hour='$hour', currentTime='$currentTime' WHERE id='$id'");
+        echo json_encode(["success"=>1]);
+        exit();
+    }
 
      // Consulta todos los registros de la tabla citas
      $sqlApointment = mysqli_query($connectionBD,"SELECT * FROM citas ");
